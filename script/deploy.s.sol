aba20d191a400cf91959849e83e43066fcded864
lxhyl <zhangpengfan6@gmail.com>
2023-01-02 17:29:44 +0800

    chore: add deploy param

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -6,14 +6,18 @@ import "forge-std/console.sol";
 
 import {LegacyProxy} from "../src/LegacyProxy.sol";
 import {Legacy} from "../src/Legacy.sol";
+import {ILegacy} from "../src/Interfaces/ILegacy.sol";
 contract MyLegacy is Script {
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Legacy legacy = new Legacy();
-        LegacyProxy legacyProxy = new LegacyProxy(address(legacy));
-        console.log("legacyProxy",address(legacyProxy));
+        address legacy = address(new Legacy());
+
+        bytes memory initCalldata = abi.encodeWithSelector(Legacy.init.selector, address(0x366858498Eb1834b4a194B7d1454a92B28c6bf7e), 4 weeks, 520 weeks, 4 weeks);
+
+        address legacyProxy = address(new LegacyProxy(legacy, initCalldata));
+        
         vm.stopBroadcast();
     }
 }
